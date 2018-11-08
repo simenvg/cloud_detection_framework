6c180dd20365580a4f10dfed829e7d2e1704235a
simenvg <simen.grini@gmail.com>
2018-11-08 16:03:19 +0100

    Maybe this will work?

--- yolo/test_yolo.py
+++ yolo/test_yolo.py
@@ -8,11 +8,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import sqlite3 as db
+import argparse
 
-
-test_images_list_file = open(os.path.join(
-    '/home/simenvg/data/tmp', "test.txt"), "r")
-image_filepaths = test_images_list_file.readlines()
 
 RED = (0, 0, 255)
 GREEN = (0, 255, 0)
@@ -75,25 +72,25 @@ def valid_detection(detected_box, gt_box, iou_thresh=0.5):
     return get_iou(detected_box, gt_box) >= iou_thresh
 
 
-# def get_precision_recall(detections, iou_thresh, confidence_thresh=0.0):
-#     true_positives = 0
-#     num_detections = 0
-#     num_gt_boxes = 0
-#     for key, value in detections.iteritems():
-#         gt_boxes = get_GT_boxes(os.path.join(
-#             '/home/simenvg/tmp/test', (key.strip()[:-4] + '.xml')))
-#         num_detections += len(value)
-#         for gt_box in gt_boxes:
-#             for detected_box in value:
-#                 if detected_box.confidence >= confidence_thresh:
-#                     if valid_detection(detected_box, gt_box, iou_thresh=iou_thresh):
-#                         true_positives += 1
-#                         value.remove(detected_box)
-#                         break
-#         num_gt_boxes += len(gt_boxes)
-#     precision = float(true_positives) / float(num_detections)
-#     recall = float(true_positives) / float(num_gt_boxes)
-#     return (precision, recall)
+def get_precision_recall2(detections, iou_thresh, confidence_thresh=0.0):
+    true_positives = 0
+    num_detections = 0
+    num_gt_boxes = 0
+    for key, value in detections.iteritems():
+        gt_boxes = get_GT_boxes(os.path.join(
+            '/home/simenvg/tmp/test', (key.strip()[:-4] + '.xml')))
+        num_detections += len(value)
+        for gt_box in gt_boxes:
+            for detected_box in value:
+                if detected_box.confidence >= confidence_thresh:
+                    if valid_detection(detected_box, gt_box, iou_thresh=iou_thresh):
+                        true_positives += 1
+                        value.remove(detected_box)
+                        break
+        num_gt_boxes += len(gt_boxes)
+    precision = float(true_positives) / float(num_detections)
+    recall = float(true_positives) / float(num_gt_boxes)
+    return (precision, recall)
 
 
 # iou_threshs = [x * 0.01 for x in range(0, 100)]
@@ -115,61 +112,48 @@ def valid_detection(detected_box, gt_box, iou_thresh=0.5):
 # plt.show()
 
 
-conn = db.connect('''detections.db''')
-c = conn.cursor()
+def save_images_with_boxes(conn, data_path):
+    c = conn.cursor()
+    i = 0
+    test_file = open(data_path, 'model', 'test.txt')
+    image_filepaths = test_file.readlines()
+    test_file.close()
+    for img in image_filepaths:
+        gt_boxes = get_GT_boxes(os.path.join(
+            '', (img.strip()[:-4] + '.xml')))
+        c.execute('SELECT * FROM detections WHERE image_name=?', (img,))
+        detections = c.fetchall()
+        # im_path = os.path.join('/home/simenvg/data/tmp/test', img.strip())
+        image = cv2.imread(img)
+        print(img)
+        if image is None:
+            print('No image')
+            exit()
+        for box in gt_boxes:
+            cv2.rectangle(image, (int(box.x_min), int(box.y_max)),
+                          (int(box.x_max), int(box.y_min)), GREEN, 2)
+        for box in detections:
+            if (box[5] == 'building'):
+                color = BLUE
+            else:
+                color = RED
+            cv2.rectangle(image, (int(box[1]), int(box[3])),
+                          (int(box[2]), int(box[4])), color, 2)
+        cv2.imwrite(os.path.join(data_path, 'results',
+                                 'image_' + str(i) + '.jpg'), image)
+        i += 1
 
 
-# get_precision_recall(YOLO_detections, 0.1)
-font = cv2.FONT_HERSHEY_SIMPLEX
-bottomLeftCornerOfText = (10, 500)
-fontScale = 0.3
-fontColor = (255, 255, 255)
-lineType = 1
-
-# print(YOLO_detections['data/obj/extra_kayak_axis0000.jpg'][0].confidence)
-i = 0
-for img in image_filepaths:
-    gt_boxes = get_GT_boxes(os.path.join(
-        '', (img.strip()[:-4] + '.xml')))
-    # detections = get_yolo_detections(img, net, meta_data_net)
-    c.execute('SELECT * FROM detections WHERE image_name=?', (img,))
-    detections = c.fetchall()
-    im_path = os.path.join('/home/simenvg/data/tmp/test', img.strip())
-    image = cv2.imread(im_path)
-    print(img)
-    if image is None:
-        print('No image')
-        exit()
-    for box in gt_boxes:
-        cv2.rectangle(image, (int(box.x_min), int(box.y_max)),
-                      (int(box.x_max), int(box.y_min)), GREEN, 2)
-    for box in detections:
-        if (box[5] == 'building'):
-            color = BLUE
-        else:
-            color = RED
-        cv2.rectangle(image, (int(box[1]), int(box[3])),
-                      (int(box[2]), int(box[4])), color, 2)
-        # cv2.putText(image, box[5],
-        #             (int(box[1]), int(box[3])),
-        #             font,
-        #             fontScale,
-        #             RED,
-        #             lineType)
-    # cv2.imshow('ime', image)
-    cv2.imwrite(os.path.join('/home/simenvg/results', 'image_' + str(i) + '.jpg'), image)
-    i += 1
-    # cv2.waitKey(0)
-conn.close()
+def main(data_path):
+    conn = db.connect(os.path.join(data_path, 'results', 'detections.db'))
+    save_images_with_boxes(conn, data_path)
+    conn.close()
 
 
-# image = cv2.imread(os.path.join("/home/simenvg/environments/my_env/test2/data/obj/extra_kayak_axis0000.jpg"))
-# And then down here you could detect a lot more images like:
-# r = dn.detect(net, meta, "data/obj/selected_08_07_frame12055.jpg")
-# print r
-# r = dn.detect(net, meta, "data/giraffe.jpg")
-# print r
-# r = dn.detect(net, meta, "data/horses.jpg")
-# print r
-# r = dn.detect(net, meta, "data/person.jpg")
-# print r
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Input path to darknet')
+    parser.add_argument('DATA_PATH', type=str, nargs=1,
+                        help='Set path to data folder, containg datasets')
+    args = parser.parse_args()
+    DATA_PATH = args.DATA_PATH[0]
+    main(DATA_PATH)
