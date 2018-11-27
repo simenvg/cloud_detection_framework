08026f8675b05c348909f0e4cb99d7e3616bcc3f
simenvg <simen.grini@gmail.com>
2018-11-27 10:45:09 +0100

    bugfix

--- yolo/test_yolo.py
+++ yolo/test_yolo.py
@@ -156,7 +156,7 @@ def main(data_path):
     prec_recall_dict['precisions'] = precisions
     prec_recall_dict['recalls'] = recalls
     prec_recall_dict['name'] = 'Yolo'
-    file = open(os.path.join(data_path, 'results', 'prec_recalls.txt'), 'w')
+    file = open(os.path.join(data_path, 'results', 'prec_recalls.txt'), 'wb')
     pickle.dump(prec_recall_dict, file)
     file.close()
     # print(get_precision_recall(conn, data_path, 0.5))
