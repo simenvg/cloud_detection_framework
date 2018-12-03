d22ea421dd777af364a6a461594f3dd28d8a538f
simenvg <simen.grini@gmail.com>
2018-12-03 12:06:24 +0100

    Choose which combinations of train and test data to plot prec recall

--- resources/plot_precision_recall.py
+++ resources/plot_precision_recall.py
@@ -3,12 +3,10 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser(description='Input path to darknet')
-parser.add_argument('DATA_PATH', type=str, nargs=1,
+parser.add_argument('results_path', type=str, nargs=1,
                     help='Set path to data folder, containg datasets')
 args = parser.parse_args()
-DATA_PATH = args.DATA_PATH[0]
-
-file_names = os.listdir(DATA_PATH)
+results_path = args.results_path[0]
 
 
 def load_file(file_path):
@@ -21,17 +19,80 @@ def load_file(file_path):
     return prec_recall_dict
 
 
-for file_name in file_names:
-    file_path = os.path.join(DATA_PATH, file_name)
-    prec_recall_dict = load_file(file_path)
-    name = prec_recall_dict['name']
-    precisions = prec_recall_dict['precisions']
-    recalls = prec_recall_dict['recalls']
-    plt.plot(recalls, precisions, label=name)
-    plt.grid(True)
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
-    plt.legend(loc="lower left")
-    plt.title('Precision/Recall curve')
+def get_files(results_path):
+    prec_recall_paths = []
+    trained_on = ['trained_on_trf_bc_bf',
+                  'trained_on_trf_bc_bf_bb_build', 'trained_on_trf_bc_bf_bbnb']
+    tested_on = ['tested_on_b_and_b', 'tested_on_trf',
+                 'tested_on_bc_bf', 'tested_on_bc_bf_trf']
+    ssd_yolo = ['results-yolo', 'results-ssd']
+    # for train in trained_on:
+    #     for test in tested_on:
+    #         for det in ssd_yolo:
+    #             prec_recall_paths.append(os.path.join(
+    #                 results_path, train, test, det))
 
-plt.show()
+    while True:
+        print('Trained on dataset:')
+        for i in range(len(trained_on)):
+            print('[' + str(i) + ']  : ' + trained_on[i])
+        print('Tested on dataset:')
+        for i in range(len(tested_on)):
+            print('[' + str(i) + ']  : ' + tested_on[i])
+        print('Detection alg:')
+        for i in range(len(ssd_yolo)):
+            print('[' + str(i) + ']  : ' + ssd_yolo[i])
+        user_input = raw_input(
+            'Type number of trained on space, then number tested on, then ssd or yolo. To quit press q.  ')
+        if user_input == 'q':
+            break
+        user_input_list = user_input.split(' ')
+        user_input_name = raw_input('name in plot: ')
+
+        prec_recall_paths.append((os.path.join(
+            results_path, trained_on[int(user_input_list[0])], tested_on[int(user_input_list[1])], ssd_yolo[int(user_input_list[2])], 'prec_recalls.txt'), user_input_name))
+    return (prec_recall_paths)
+
+
+# def get_files2(results_path):
+#     i = 1
+#     trained_on = ['trained_on_trf_bc_bf',
+#                   'trained_on_trf_bc_bf_bb_build', 'trained_on_trf_bc_bf_bbnb']
+#     tested_on = ['tested_on_b_and_b', 'tested_on_trf',
+#                  'tested_on_bc_bf', 'tested_on_bc_bf_trf']
+#     ssd_yolo = ['results-yolo', 'results-ssd']
+#     for train in trained_on:
+#         for test in tested_on:
+#             print(train, test)
+#             prec_recall_out_paths = [os.path.join(results_path, train, test, ssd_yolo[0], 'prec_recalls.txt'), os.path.join(
+#                 results_path, train, test, ssd_yolo[1], 'prec_recalls.txt')]
+#             plot_prec_recall(prec_recall_out_paths, os.path.join(
+#                 results_path, train, test, 'prec_recall.png'), i)
+#             i += 1
+
+
+def plot_prec_recall(prec_recall_file_paths, output_path, i):
+    print('paths: ', prec_recall_file_paths)
+    for file_path in prec_recall_file_paths:
+        prec_recall_dict = load_file(file_path[0])
+        name = file_path[1]
+        precisions = prec_recall_dict['precisions']
+        recalls = prec_recall_dict['recalls']
+        plt.figure(i)
+        plt.plot(recalls, precisions, label=name)
+        plt.grid(True)
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.ylim(top=1.03)
+        plt.ylim(bottom=0)
+        plt.legend(loc="lower left")
+        plt.title('Precision/Recall curve')
+
+    plt.show()
+    # plt.savefig(output_path)
+
+
+output_path = '/home/simenvg/'
+
+files = get_files(results_path)
+plot_prec_recall(files, output_path, 1)
